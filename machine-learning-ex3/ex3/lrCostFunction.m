6be310e2b7b7a105835e268cff7cb4b5afeda732
Matsuo <soshi.matsuo@p30443.intra.rakuten.co.jp>
2018-10-19 12:52:41 +0900

    cost function of logisticF

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -35,9 +35,21 @@ grad = zeros(size(theta));
 %           temp(1) = 0;   % because we don't add anything for j = 0  
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
+z = sigmoid(X*theta);
 
 
+J_u = (-1/m) * sum( y .* log(z) + (1.-y) .* log(1 .- z))
 
+grad_u = 1/m * (X' * (z - y));
+
+% reguralize
+
+temp = theta
+temp(1)=0
+
+regtest = (lambda / (2 * m)) * sum(temp.^2)
+J = J_u + regtest
+grad = grad_u + (lambda/m) .* temp;
 
 
 
