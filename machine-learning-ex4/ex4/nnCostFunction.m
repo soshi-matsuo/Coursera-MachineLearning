6b6c4e7fab875546c561272a5e183540b2261e79
Matsuo <soshi.matsuo@p30443.intra.rakuten.co.jp>
2018-10-23 12:47:07 +0900

    Cost function

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -62,13 +62,30 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+% Forward prop and Cons function
+y_matrix= eye(num_labels)(y,:);
+
+a1 = [ones(m,1) X];
+
+z2 = a1 * Theta1';
+a2 = sigmoid(z2);
+a2 = [ones(m,1) a2];
+
+z3 = a2 * Theta2';
+a3 = sigmoid(z3);
+
+J = (1/m) * sum(sum( -y_matrix .* log(a3) - (1-y_matrix) .* log(1-a3)));
 
 
+% Regularized Const function
+nrJ = (1/m) * sum(sum( -y_matrix .* log(a3) - (1-y_matrix) .* log(1-a3)));
+tmp1 = Theta1;
+tmp2 = Theta2;
+tmp1(:,1) = 0; 
+tmp2(:,1) = 0;
+J =  nrJ + (lambda/(2*m)) * (sum(sum(tmp1.^2)) + sum(sum(tmp2.^2)));
 
-
-
-
-
+% Back prop 
 
 
 
